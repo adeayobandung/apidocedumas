2d425f4cdf7fd4bc99b6ea262c5ea45b83a01726
Ade Indra Saputra <adeayobandung@gmail.com>
2020-09-05 00:58:34 +0700

    add lp

--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-04T17:55:42.792Z",
+    "time": "2020-09-04T17:58:16.772Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
