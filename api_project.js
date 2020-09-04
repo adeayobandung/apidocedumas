d08bd7bf2d204535238d1e60f6b2cbbd7e6f8517
Ade Indra Saputra <adeayobandung@gmail.com>
2020-09-05 00:56:22 +0700

    add laporkan polisi

--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-04T16:53:34.399Z",
+    "time": "2020-09-04T17:55:42.792Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
